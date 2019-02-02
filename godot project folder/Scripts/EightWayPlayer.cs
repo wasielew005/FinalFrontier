0402c4b7882180f3377b009e3d935c650045400b
flowers.adelai <flowers.adelai@gmail.com>
2019-02-02 10:33:19 -0500

    wasd movement

---
+++ godot project folder/Scripts/EightWayPlayer.cs
@@ -0,0 +1,102 @@
+using Godot;
+using System;
+
+public class EightWayPlayer : KinematicBody2D
+{
+    [Export] public int Speed = 50;
+
+    Vector2 velocity = new Vector2();
+	
+	int counter = 0;
+	
+
+    public void GetInput()
+    {   //prite = GetNode("sprite");
+        velocity = new Vector2();
+        if (Input.IsActionPressed("right"))
+        {   
+            velocity.x += 1;
+			Rotation = 1.5708f;
+			
+			
+        }
+        if (Input.IsActionPressed("left"))
+        {   
+			
+            velocity.x -= 1;
+			Rotation = 4.71239f;
+			
+        }
+        if (Input.IsActionPressed("down"))
+        {
+            
+			velocity.y += 1;
+			Rotation = 3.14159f;
+			if (Input.IsActionPressed("right"))
+        	{   
+            	velocity.x += 1;
+			    Rotation = 2.35619f;
+			
+        	}
+        	if (Input.IsActionPressed("left"))
+        	{   
+			
+            	velocity.x -= 1;
+				Rotation= 3.92699f;
+        	}
+			
+        }
+        if (Input.IsActionPressed("up"))
+        {
+            
+			velocity.y -= 1;
+			Rotation = 0;
+			if (Input.IsActionPressed("right"))
+        	{   
+            	velocity.x += 1;
+			    Rotation = 0.785398f;
+			
+        	}
+        	if (Input.IsActionPressed("left"))
+        	{   
+			
+            	velocity.x -= 1;
+				Rotation= 5.49779f;
+        	}
+			
+        }
+//		else if (Input.IsActionPressed("right") && Input.IsActionPressed("up"))
+//        {   
+//
+//            velocity.x += 1;
+//			velocity.y -= 1;
+//
+//        }
+//        else if (Input.IsActionPressed("down") && Input.IsActionPressed("right"))
+//        {
+//
+//			velocity.y += 1;
+//			velocity.x += 1;
+//
+//        }
+//        else if (Input.IsActionPressed("down") && Input.IsActionPressed("left"))
+//        {
+//
+//			velocity.x -= 1;
+//			velocity.y += 1;
+//
+//        }
+//		else if (Input.IsActionPressed("up") && Input.IsActionPressed("left")){
+//		    velocity.x -= 1;
+//			velocity.y -= 1;
+//		}
+        velocity = velocity.Normalized() * Speed;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        MoveAndSlide(velocity);
+		
+	}
+}
