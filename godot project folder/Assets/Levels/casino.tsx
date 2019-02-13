f3f9f6cbfd1389f01569d097f7512e502af808b7
Brett <brettlesczynski@outlook.com>
2019-02-13 14:40:09 -0500

    Fixed Imports

--- godot project folder/Assets/Levels/casino.tsx
+++ godot project folder/Assets/Levels/casino.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="casino" tilewidth="32" tileheight="32" tilecount="256" columns="8">
- <image source="../../../../../../Dropbox/School/College/2018 - 2019 Senior/Spring/Senior Design/casinotileset.png" width="256" height="1024"/>
+ <image source="casinotileset.png" width="256" height="1024"/>
 </tileset>
