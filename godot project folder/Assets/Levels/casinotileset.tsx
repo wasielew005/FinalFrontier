0768ab93920c869649bc7cafd4cc68a462ecf4de
Brett <brettlesczynski@outlook.com>
2019-02-21 16:37:40 -0500

    New test map with collision detection

---
+++ godot project folder/Assets/Levels/casinotileset.tsx
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="casinotileset" tilewidth="32" tileheight="32" tilecount="256" columns="8">
+ <image source="casinotileset.png" width="256" height="1024"/>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="-0.125" width="31.5" height="31.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index">
+   <object id="1" x="3.75" y="13" width="28.25" height="18.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="12.875" width="31.625" height="18.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="13" width="26.625" height="19.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index">
+   <object id="1" x="4.375" y="0.125" width="27.375" height="31.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="26.5" height="31.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="104">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="0.125" width="28.125" height="17.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index">
+   <object id="1" x="0.375" y="-0.125" width="31.375" height="17.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="27.25" height="17.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="187">
+  <objectgroup draworder="index">
+   <object id="1" x="9.875" y="15" width="22" height="16.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="188">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="15.125" width="31.75" height="16.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="189">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="17" width="32.375" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="190">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="15.125" width="31.5" height="16.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="15" width="22" height="17.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="195">
+  <objectgroup draworder="index">
+   <object id="1" x="9.875" y="0.25" width="22" height="31.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="196">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="-0.125" width="31.875" height="31.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="197">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31.75" height="31.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="198">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="-0.125" width="32" height="31.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="199">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0.125" width="21.75" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index">
+   <object id="3" x="4.25" y="0.75" width="26.375" height="31"/>
+  </objectgroup>
+ </tile>
+</tileset>
