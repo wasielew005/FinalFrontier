bb13e3eaed4b3f4047c1b31b4a756e2f2b2a365c
Kyler <kylerbratt42@gmail.com>
2019-02-23 12:32:11 -0500

    shooting part 2

--- godot project folder/Assets/Levels/kylerstestarea.gd
+++ godot project folder/Assets/Levels/kylerstestarea.gd
@@ -1,15 +1,14 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var explosion = preload("res://Instances/Effects/Explosion.tscn")
+
 onready var player = get_node("Player")
 
 func _ready():
 	player.connect("hit", self, "show_hit")
 	
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func show_hit(hit_location):
+	var smoke = explosion.instance()
+	add_child(smoke)
+	smoke.set_position(hit_location)
+	smoke.set_emitting(true)
