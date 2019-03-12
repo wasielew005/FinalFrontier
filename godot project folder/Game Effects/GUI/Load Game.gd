33293505231a68337cd01d5fe9fbb6ca27d3ef7f
Kyler <kylerbratt42@gmail.com>
2019-03-12 12:23:42 -0400

    comment and millisecond calc tweaks

--- godot project folder/Game Effects/GUI/Load Game.gd
+++ godot project folder/Game Effects/GUI/Load Game.gd
@@ -17,4 +17,6 @@ func _ready():
 
 func _on_Load_Game_pressed():
 	get_tree().change_scene("res://Assets/Levels/kylerstestarea.tscn")
+	#global._get_time() must be called in any button that loads into the game
+	#it will get the beginning time which is used in timekeeper.gd to calculate elapsed time
 	global._get_time()
--- godot project folder/Scripts/global.gd
+++ godot project folder/Scripts/global.gd
@@ -9,6 +9,7 @@ extends Node
 var time_start = 0
 var time_now = 0
 var str_elapsed = ""
+var millisec_start = 0
 var millisec_now = 0
 
 #Determines whether the character is reloading
@@ -21,14 +22,14 @@ var value = 0
 var ammo_in_weapon = 6
 
 
-
-
 func _ready():
 	pass
-	
+
+
 #sets the beginning time to be calculated in the timekeeper.gd script
 func _get_time():
     time_start = OS.get_unix_time()
+    millisec_start = OS.get_ticks_msec()
     set_process(true)
 
 
