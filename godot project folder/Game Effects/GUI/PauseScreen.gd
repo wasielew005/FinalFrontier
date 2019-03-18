03a4523d6fd59e8ec6f907299c0120a2f0aaf59d
Brett <lesczyns001@knights.gannon.edu>
2019-03-18 16:33:01 -0400

    Pause Menu Created

--- godot project folder/Game Effects/GUI/PauseScreen.gd
+++ godot project folder/Game Effects/GUI/PauseScreen.gd
@@ -6,14 +6,11 @@ func _ready():
 	$MarginContainer/CenterContainer/VBoxContainer/Resume.grab_focus()
 
 func _physics_process(delta):
-	if $MarginContainer/CenterContainer/VBoxContainer/Resume.is_hovered() == true:
-		$MarginContainer/CenterContainer/VBoxContainer/Resume.grab_focus()
-	if $MarginContainer/CenterContainer/VBoxContainer/Quit.is_hovered() == true:
-		$MarginContainer/CenterContainer/VBoxContainer/Quit.grab_focus()
+	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		$MarginContainer/CenterContainer/VBoxContainer/Quit.grab_focus()
+		$MarginContainer/CenterContainer/VBoxContainer/Resume.grab_focus()
 		get_tree().paused = not get_tree().paused
 		visible = not visible
 
@@ -21,9 +18,25 @@ func _on_Resume_pressed():
 	get_tree().paused = not get_tree().paused
 	visible = not visible
 	
+func _on_Buy_pressed():
+	#get_node("/root/menumusic").play()
+	$buypopup.show()
+
 func _on_Quit_pressed():
 	get_tree().paused = not get_tree().paused
 	visible = not visible
 	get_node("/root/menumusic").play()
 	get_tree().change_scene("res://Game Effects/GUI/MainMenu.tscn")
-	
+
+
+func _on_close_pressed():
+	$buypopup.hide()
+
+
+func _on_MuteSound_pressed():
+	if !AudioServer.is_bus_mute(0):
+		$MarginContainer/CenterContainer/VBoxContainer/MuteSound/mute.set_text("Unmute")
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+		$MarginContainer/CenterContainer/VBoxContainer/MuteSound/mute.set_text("Mute")
