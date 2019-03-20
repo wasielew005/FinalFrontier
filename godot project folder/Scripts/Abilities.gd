5e5cdbef9c1c60bfbf4190ca21157c9e78f8af9d
theseapug <hkelly1129@gmail.com>
2019-03-20 14:02:16 -0400

    small ability additions. need timers

--- godot project folder/Scripts/Abilities.gd
+++ godot project folder/Scripts/Abilities.gd
@@ -6,22 +6,42 @@ func _ready():
 #in PauseScreen, use this to test
 #var player = get_parent().get_parent()
 #player.speed = abilities.double_speed(player.speed)
+
+#var t = Timer.new()
+#t.set_wait_time(1)
+#t.set_one_shot(true)
+#self.add_child(t)
+#t.start()
+#yield(t, "timeout")
+
 func double_speed(var speed):
-	speed = speed * 3
-	print("Triple speed!")
+	if Input.is_key_pressed("ui_3"):
+		
+		speed = speed * 3
+		print("Triple speed!")
 	return speed
 	
 func medpack(var playerHealth):
-	if(playerHealth < maxHealth):
-		playerHealth = maxHealth
-		print("Health restored!")
+	if Input.is_key_pressed("ui_4"):
+		if(playerHealth < maxHealth):
+			playerHealth = maxHealth
+			print("Health restored!")
+		else:
+			print("Health is already full")
 	return playerHealth
 	
-func double_damage():
-	pass
+func double_damage(var playerDamage):
+	if Input.is_key_pressed("ui_5"):
+		playerDamage = playerDamage * 2
+	return playerDamage
 	
 func invisibility():
+	
 	pass
 	
 func invincibility():
+	
+	pass
+	
+func silencer():
 	pass
