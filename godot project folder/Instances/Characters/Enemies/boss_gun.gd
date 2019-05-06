a1e656131f8f97ff06d3588de171446f08d176b6
Andrew Burris <aburris8950@gmail.com>
2019-05-05 20:44:10 -0400

    adding some level assets

---
+++ godot project folder/Instances/Characters/Enemies/boss_gun.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var bullet = preload("boss_projectile.tscn")
+onready var spawn = get_parent().get_node("sprite_head").get_node("spawn")
+
+func fire(angle):
+	var new_bullet = bullet.instance()
+	new_bullet.position = spawn.global_position
+	get_parent().get_parent().add_child(new_bullet)
