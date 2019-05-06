d988aa98e7b4e1bf5cb2af1aeb7aa701ba8571bb
Andrew Burris <aburris8950@gmail.com>
2019-05-06 17:23:27 -0400

    Maze level

---
+++ godot project folder/Assets/Levels/andrew_levels/assets/kylertilesresize/kylertilesresize.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="kylertilesresize" tilewidth="64" tileheight="64" tilecount="26" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="resize_black-1.png.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="resize_ground4-1.png.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="resize_kylerslotmachine.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="resize_kylerslotmachine-stool.png.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="resize_kylerstairs.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="resize_kylerstool.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="resize_kylertile-1.png.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="resize_pokertabletlc-1.png(2)-1.png(1).png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="resize_pokertabletlc-1.png(2)-1.png(2).png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="resize_pokertabletlc-1.png(2)-1.png(3).png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="resize_pokertabletlc-1.png(2)-1.png.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="resize_table.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="resize_table2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="resize_table3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="resize_wall1-1.png.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="resize_wallbcap-1.png.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="resize_wallblc-1.png.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="resize_wallbrc-1.png.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="resize_wallh-1.png.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="resize_walllcap-1.png.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="resize_wallrcap-1.png.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="resize_wallsplit1-1.png.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="resize_walltcap-1.png.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="resize_walltlc-1.png.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="resize_walltrc-1.png.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="resize_white-1.png.png"/>
+ </tile>
+</tileset>
