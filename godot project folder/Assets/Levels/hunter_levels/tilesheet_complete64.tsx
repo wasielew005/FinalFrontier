4b081c97ebb964349537d1f011d8f66e1f031982
Hunter <huntemup95@gmail.com>
2019-05-13 23:44:46 -0400

    Hunter's Levels

---
+++ godot project folder/Assets/Levels/hunter_levels/tilesheet_complete64.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tilesheet_complete64" tilewidth="64" tileheight="64" tilecount="540" columns="27">
+ <image source="tilesheet_complete.png" width="1728" height="1280"/>
+ <tile id="108">
+  <objectgroup draworder="index">
+   <object id="4" x="0" y="0" width="63" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="109">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0" width="62" height="66"/>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1" width="64" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="63" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="112">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="62" height="62"/>
+  </objectgroup>
+ </tile>
+ <tile id="114">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-1" width="66" height="66"/>
+  </objectgroup>
+ </tile>
+ <tile id="135">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="65" height="63"/>
+  </objectgroup>
+ </tile>
+ <tile id="136">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0" width="63" height="65"/>
+  </objectgroup>
+ </tile>
+ <tile id="137">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-1" width="64" height="66"/>
+  </objectgroup>
+ </tile>
+ <tile id="138">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="63" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="139">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="64" height="66"/>
+  </objectgroup>
+ </tile>
+ <tile id="140">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1" width="63" height="65"/>
+  </objectgroup>
+ </tile>
+ <tile id="141">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1" width="64" height="62"/>
+  </objectgroup>
+ </tile>
+ <tile id="169">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="450">
+  <objectgroup draworder="index">
+   <object id="4" x="8.66667" y="10.3333">
+    <polygon points="0,0 11,-0.333333 13,1.66667 34,2 35.3333,-1 45.3333,-0.666667 47.3333,1.66667 47.3333,23.6667 46.3333,30 42,37.3333 36.3333,41 31.6667,42.3333 14.3333,42.3333 9.66667,40.6667 4.66667,37.3333 1.33333,32.3333 -1,25.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="500">
+  <objectgroup draworder="index">
+   <object id="1" x="63.6667" y="11">
+    <polygon points="0,0 -49.3333,-0.333333 -53,4.33333 -57.3333,4.33333 -58.3333,37.6667 -54.3333,42.6667 -0.333333,42.6667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="501">
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="10.6667" width="63.6667" height="42.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="502">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.333333" y="10.3333">
+    <polygon points="0,0 0,43.3333 54.3333,43.6667 58.6667,39.3333 58.6667,5.66667 52,5.66667 49,0.333333"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="504">
+  <objectgroup draworder="index">
+   <object id="1" x="14.3333" y="10.3333">
+    <polygon points="0,0 -3.33333,5.33333 -8.66667,5.33333 -8.33333,32.3333 -8.33333,39 -5.66667,43 39.6667,43.6667 43.6667,39.3333 42.6667,6 40.6667,5 37.6667,5.33333 36.6667,1.33333 33.6667,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
